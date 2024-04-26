f5f94d9290f8ffd87b7eccf53da369fb3a8bd831
Christen <christen.foad@gmail.com>
2024-04-26 10:54:50 +0300

    Add project files.

---
+++ CompanyBlazor/Pages/FirstComponent.razor.cs
@@ -0,0 +1,17 @@
+﻿namespace CompanyBlazor.Pages
+{
+    public partial class FirstComponent
+    {
+        public int No { get; set; }
+        public string Msg { get; set; } = "Hiiiiiiiii";
+        //C#
+        void ShowMessage(string msg)
+        {
+            Console.WriteLine($"Hi From C# Handel Click Event {msg}");
+        }
+        void Change()
+        {
+            Msg = "byyyyyyyyyyyyy";
+        }
+    }
+}
