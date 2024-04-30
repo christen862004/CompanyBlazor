e36924ac98a34659f698387f1d8240bd63d31baa
Christen <christen.foad@gmail.com>
2024-04-30 13:32:32 +0300

    Day3 Validation Part

--- CompanyBlazor/Models/Employee.cs
+++ CompanyBlazor/Models/Employee.cs
@@ -1,9 +1,16 @@
-﻿namespace CompanyBlazor.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CompanyBlazor.Models
 {
     public class Employee
     {
         public int Id { get; set; }
+
+        [MinLength(3,ErrorMessage ="Name Must Be More Than 2 Letter")]
+        [MaxLength(25,ErrorMessage ="Name Must BE Less Than 25 letter")]
         public string Name { get; set; }
+
+        [Range(6000,25000)]
         public int Salary { get; set; }
         public string Image { get; set; }
         public int Dept_Id { get; set; }
